278ef040fb51864fd416b701b9d0c7cf4eb2a647
RoaringInferno <jj2t17@gmail.com>
2024-10-13 20:59:33 -0500

    Draft Q2

--- q2.cpp
+++ q2.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-using namespace std;
+/**
+ * I would create 3 classes: TempKelvin, TempCelsius, and TempFahrenheit.
+ * Each class would have a private member variable to store the temperature in the respective unit.
+ * Each class would have a getter and setter for their private member variable, and a value constructor.
+ * Then, each class would have a copy constructor and conversion constructor and operator.
+ * They would have private inline static member functions to convert between the units, so that the formulas are centralized, but performance is not lost.
+ */
 
 class Temperature {
 	private:
@@ -9,13 +15,23 @@ class Temperature {
 		       degreesCelsius;
 	public:
 		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		double getTempKelvin() { return degreesKelvin; }
+		void setTempCelsius() { double degreesCelsius = degreesKelvin - 273.15; }
+		double getTempCelsius() { return degreesCelsius; } 
+		void setTempFahrenheit() { double degreesFahrenheit = degreesCelsius * 9.0 / 5.0 + 32.0; }
+		double getTempFahrenheit() { return degreesFahrenheit; }
 };
 
 int main() {
+	Temperature temp;
 
+	double kelvin;
+
+	std::cout << "Enter the temperature in Kelvin: ";
+	std::cin >> kelvin;
+
+	temp.setTempKelvin(kelvin);
+	std::cout << "Temperature in Kelvin: " << temp.getTempKelvin() << "\n";
+	std::cout << "Temperature in Celsius: " << temp.getTempCelsius() << "\n";
+	std::cout << "Temperature in Fahrenheit: " << temp.getTempFahrenheit() << "\n";
 }
