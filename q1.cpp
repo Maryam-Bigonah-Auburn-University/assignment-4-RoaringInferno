444a2d6a968a255ce8d7f5ba149bbf6e15567d99
RoaringInferno <jj2t17@gmail.com>
2024-10-13 20:54:39 -0500

    Draft Q1

--- q1.cpp
+++ q1.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-#include <iomanip>
-
-using namespace std;
 
 class GasPump {
-	private:
+	private: // Constants
+		static const double GAS_DISPENSE_RATE = 0.1;
+	private: // Members
 		double amountDispensed,
 		       amountCharged,
 		       costPerGallon;
 
-	public:
+	public: // Constructors
+		GasPump() :
+			amountDispensed(0.0),
+			amountCharged(0.0),
+			costPerGallon(0.0)
+		{}
+	public: // Methods
 		double getAmountDispensed() { return amountDispensed; }
 		double getAmountCharged() { return amountCharged; }
 		double getCostPerGallon() { return costPerGallon; }
 		void setCostPerGallon(double cpg) { costPerGallon = cpg; }
         
 		void dispense(int seconds) {
-
-
-			
-			amountCharged = amountDispensed * costPerGallon;
+			double amountDispensedThisPump = GAS_DISPENSE_RATE * seconds;
+			amountDispensed += amountDispensedThisPump;
+			amountCharged += amountDispensedThisPump * costPerGallon;
 		}
 		void reset() {
-            
 			amountDispensed = 0.0;
 			amountCharged = 0.0;
 		}		
@@ -31,11 +34,24 @@ class GasPump {
 int main() {
 	
     //Define variables
+	GasPump pump1;
 
     // Define Class Object
+	double pricepergalloninput;
+	int secondsinput;
+	std::cout << "Enter the price per gallon: ";
+	std::cin >> pricepergalloninput;
+	std::cout << "Enter the number of seconds to dispense: ";
+	std::cin >> secondsinput;
 
     // Call Functions
-	
+	pump1.setCostPerGallon(pricepergalloninput);
+	pump1.dispense(secondsinput);
+
+	// Output
+	std::cout << "Amount dispensed: " << pump1.getAmountDispensed() << "\n";
+	std::cout << "Cost per gallon: " << pump1.getCostPerGallon() << "\n";
+	std::cout << "Amount charged: " << pump1.getAmountCharged() << "\n";
 
 	return 0;
 }
